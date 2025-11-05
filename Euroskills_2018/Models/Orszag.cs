4eda9c7407c95544742a1f0ba257d992c0dfd9a3
DavidC112 <cornutadavid0@gmail.com>
2025-11-05 12:58:34 +0100

    Models

---
+++ Euroskills_2018/Models/Orszag.cs
@@ -0,0 +1,9 @@
+﻿namespace Euroskills_2018.Models
+{
+    public class Orszag
+    {
+        public int Id { get; set; }
+        public string Orszagnev { get; set; }
+        public ICollection<Versenyzo> Versenyzok { get; set; }
+    }
+}
---
+++ Euroskills_2018/Models/Versenyzo.cs
@@ -0,0 +1,11 @@
+﻿namespace Euroskills_2018.Models
+{
+    public class Versenyzo
+    {
+        public int Id { get; set; }
+        public string nev { get; set; }
+        public string SzakmaId { get; set; }
+        public string OrszagId { get; set; }
+        public int pont { get; set; }
+    }
+}
